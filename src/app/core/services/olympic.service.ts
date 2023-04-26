bd7e2acbf44fd3f857a32bac311ccc176a7a5917
MONPCDECOMPET\Thomas MICHEL <michelt.thomas@gmail.com>
2023-04-26 11:23:05 +0200

    feat: Improve error handling in Olympic service with custom handleError method

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -7,6 +7,8 @@ import { Participation } from '../models/Participation';
 import { __values } from 'tslib';
 import { map } from 'rxjs/operators';
 import { Observable, of } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -20,13 +22,31 @@ export class OlympicService {
   loadInitialData() {
     return this.http.get<Olympic[]>(this.olympicUrl).pipe(
       tap((value) => this.olympics$.next(value)),
-      catchError((error, caught) => {
-        // TODO: improve error handling
-        console.error(error);
-        // can be useful to end loading state and let the user know something went wrong
+      catchError((error: HttpErrorResponse, caught) => {
+        this.handleError(error);
+        // Inform the user that something went wrong by updating the observable with an empty array
         this.olympics$.next([]);
+        // return to the original observable to coninue the chain of observables
         return caught;
       })
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `,
+        error.error
+      );
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      () => new Error('Something bad happened; please try again later.')
     );
   }
 
